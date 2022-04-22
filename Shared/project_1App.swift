f5d0c7e42288c16b3551fb1a755f8fdcc5e8ecc7
Tunc <78280107+TuncG@users.noreply.github.com>
2022-04-22 17:12:07 -0400

    Initial Commit

---
+++ Shared/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  Shared
+//
+//  Created by Tunc Gonel on 2022-04-22.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
